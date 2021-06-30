3989d35e88aebf1ec2dcee6d10bbb7033e9fc867
LinkunGao <gaolinkun123@gmail.com>
2021-06-30 13:39:12 +1200

    create developer table

--- src/pages/Developers.js
+++ src/pages/Developers.js
@@ -1,7 +1,7 @@
 import { Helmet } from 'react-helmet';
 import { Box, Container } from '@material-ui/core';
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -12,36 +12,115 @@ import Paper from '@material-ui/core/Paper';
 
 const useStyles = makeStyles({
   table: {
-    minWidth: 650,
+    minWidth: 650
   },
+  headerCell: {
+    color: 'black'
+  },
+  container: {
+    marginTop: 20
+  }
 });
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white
+  },
+  body: {
+    fontSize: 14
+  }
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover
+    }
+  }
+}))(TableRow);
 
 function createData(name, calories, fat, carbs, protein) {
   return {
-    name, calories, fat, carbs, protein
+    name,
+    calories,
+    fat,
+    carbs,
+    protein
   };
 }
 const api = {
   auth: 'https://nzcsa-backend.herokuapp.com/api/auth/',
   private: 'https://nzcsa-backend.herokuapp.com/api/private/',
-  admin: 'https://nzcsa-backend.herokuapp.com/api/admin/',
+  admin: 'https://nzcsa-backend.herokuapp.com/api/admin/'
 };
 
-const rows = [
-  createData(`${api.auth}register`, 'POST', '/', '{"firstname": "melo","lastname": "guan","email": "melo.weqw@nzcsa.com","password": 123}'),
-  createData(`${api.auth}login`, 'POST', '/', '{"email": "melo.guan@nzcsa.com","password": "1234"}'),
-  createData(`${api.auth}forgotpassword`, 'POST', '/', '{"email": "meloguan123@gmail.com"}'),
-  createData(`${api.auth}resetpassword`, 'PUT', 'resetpwdToken', '{"password": 123}'),
+const rowsAuth = [
+  createData(
+    `${api.auth}register`,
+    'POST',
+    '/',
+    '{"firstname": "melo","lastname": "guan","email": "melo.weqw@nzcsa.com","password": 123}'
+  ),
+  createData(
+    `${api.auth}login`,
+    'POST',
+    '/',
+    '{"email": "melo.guan@nzcsa.com","password": "1234"}'
+  ),
+  createData(
+    `${api.auth}forgotpassword`,
+    'POST',
+    '/',
+    '{"email": "meloguan123@gmail.com"}'
+  ),
+  createData(
+    `${api.auth}resetpassword`,
+    'PUT',
+    'resetpwdToken',
+    '{"password": 123}'
+  )
+];
+
+const rowsPrivate = [
   createData(`${api.private}get-events-info`, 'GET', '/', '/'),
   createData(`${api.private}sign-up-membership`, 'POST', 'authToken', '/'),
   createData(`${api.private}get-user-info`, 'GET', 'authToken', '/'),
-  createData(`${api.private}sign-up-event`, 'POST', 'authToken', '{"eventId": "60c6d82c9551fe3b634b7b10"}'),
-  createData(`${api.admin}add-events`, 'POST', 'authToken', '{eventName, eventLocation, eventDescription, time, eventPrice, eventImgUrl}'),
-  createData(`${api.admin}delete-events`, 'DELETE', 'authToken', '{"_id": "60c70c334441e941914988e0"}'),
+  createData(
+    `${api.private}sign-up-event`,
+    'POST',
+    'authToken',
+    '{"eventId": "60c6d82c9551fe3b634b7b10"}'
+  )
+];
+
+const rowsAdmin = [
+  createData(
+    `${api.admin}add-events`,
+    'POST',
+    'authToken',
+    '{eventName, eventLocation, eventDescription, time, eventPrice, eventImgUrl}'
+  ),
+  createData(
+    `${api.admin}delete-events`,
+    'DELETE',
+    'authToken',
+    '{"_id": "60c70c334441e941914988e0"}'
+  ),
   createData(`${api.admin}show-event-user-info`, 'GET', 'authToken', '/'),
   createData(`${api.admin}show-member-list`, 'GET', 'authToken', '/'),
-  createData(`${api.admin}promo-to-member`, 'POST', 'authToken', '{ "userId": "60d04e9b8ffae49fc174a3fa" }'),
-  createData(`${api.admin}delete-member`, 'DELETE', 'authToken', '{ "userId": "60d04e9e8ffae49fc174a3fb" }'),
+  createData(
+    `${api.admin}promo-to-member`,
+    'POST',
+    'authToken',
+    '{ "userId": "60d04e9b8ffae49fc174a3fa" }'
+  ),
+  createData(
+    `${api.admin}delete-member`,
+    'DELETE',
+    'authToken',
+    '{ "userId": "60d04e9e8ffae49fc174a3fb" }'
+  )
 ];
 
 const DeveloperView = () => {
@@ -58,28 +137,158 @@ const DeveloperView = () => {
           py: 3
         }}
       >
-        <Container maxWidth="lg">
+        <Container maxWidth="lg" className={classes.container}>
           <TableContainer component={Paper}>
+            {/* auth */}
             <Table className={classes.table} aria-label="simple table">
               <TableHead>
                 <TableRow>
-                  <TableCell>API</TableCell>
-                  <TableCell align="right">Method</TableCell>
-                  <TableCell align="right">Token</TableCell>
-                  <TableCell align="right">Body Example</TableCell>
+                  <TableCell width="40%" className={classes.headerCell}>
+                    Auth API
+                  </TableCell>
+                  <TableCell
+                    align="right"
+                    width="10%"
+                    className={classes.headerCell}
+                  >
+                    Method
+                  </TableCell>
+                  <TableCell
+                    align="right"
+                    width="10%"
+                    className={classes.headerCell}
+                  >
+                    Token
+                  </TableCell>
+                  <TableCell
+                    align="right"
+                    width="40%"
+                    className={classes.headerCell}
+                  >
+                    Body Example
+                  </TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
-                {rows.map((row) => (
-                  <TableRow key={row.name}>
-                    <TableCell component="th" scope="row">
+                {rowsAuth.map((row) => (
+                  <StyledTableRow key={row.name}>
+                    <StyledTableCell component="th" scope="row">
                       {row.name}
-                    </TableCell>
-                    <TableCell align="right">{row.calories}</TableCell>
-                    <TableCell align="right">{row.fat}</TableCell>
-                    <TableCell align="right">{row.carbs}</TableCell>
-                    <TableCell align="right">{row.protein}</TableCell>
-                  </TableRow>
+                    </StyledTableCell>
+                    <StyledTableCell align="right">
+                      {row.calories}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">{row.fat}</StyledTableCell>
+                    <StyledTableCell align="right">{row.carbs}</StyledTableCell>
+                    <StyledTableCell align="right">
+                      {row.protein}
+                    </StyledTableCell>
+                  </StyledTableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Container>
+        <Container maxWidth="lg" className={classes.container}>
+          <TableContainer component={Paper}>
+            {/* private */}
+            <Table className={classes.table} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell width="40%" className={classes.headerCell}>
+                    Private API
+                  </TableCell>
+                  <TableCell
+                    align="right"
+                    width="10%"
+                    className={classes.headerCell}
+                  >
+                    Method
+                  </TableCell>
+                  <TableCell
+                    align="right"
+                    width="10%"
+                    className={classes.headerCell}
+                  >
+                    Token
+                  </TableCell>
+                  <TableCell
+                    align="right"
+                    width="40%"
+                    className={classes.headerCell}
+                  >
+                    Body Example
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rowsPrivate.map((row) => (
+                  <StyledTableRow key={row.name}>
+                    <StyledTableCell component="th" scope="row">
+                      {row.name}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">
+                      {row.calories}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">{row.fat}</StyledTableCell>
+                    <StyledTableCell align="right">{row.carbs}</StyledTableCell>
+                    <StyledTableCell align="right">
+                      {row.protein}
+                    </StyledTableCell>
+                  </StyledTableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Container>
+
+        <Container maxWidth="lg" className={classes.container}>
+          <TableContainer component={Paper}>
+            {/* admin */}
+            <Table className={classes.table} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell width="40%" className={classes.headerCell}>
+                    Admin API
+                  </TableCell>
+                  <TableCell
+                    align="right"
+                    width="10%"
+                    className={classes.headerCell}
+                  >
+                    Method
+                  </TableCell>
+                  <TableCell
+                    align="right"
+                    width="10%"
+                    className={classes.headerCell}
+                  >
+                    Token
+                  </TableCell>
+                  <TableCell
+                    align="right"
+                    width="40%"
+                    className={classes.headerCell}
+                  >
+                    Body Example
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rowsAdmin.map((row) => (
+                  <StyledTableRow key={row.name}>
+                    <StyledTableCell component="th" scope="row">
+                      {row.name}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">
+                      {row.calories}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">{row.fat}</StyledTableCell>
+                    <StyledTableCell align="right">{row.carbs}</StyledTableCell>
+                    <StyledTableCell align="right">
+                      {row.protein}
+                    </StyledTableCell>
+                  </StyledTableRow>
                 ))}
               </TableBody>
             </Table>
