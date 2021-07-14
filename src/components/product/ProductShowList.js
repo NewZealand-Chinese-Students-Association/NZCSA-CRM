bf292f3cd99a135a1692fb23bfcf9d9488d7b0aa
LinkunGao <gaolinkun123@gmail.com>
2021-07-15 01:56:05 +1200

    achieve all

--- src/components/product/ProductShowList.js
+++ src/components/product/ProductShowList.js
@@ -31,18 +31,6 @@ const StyledTableRow = withStyles((theme) => ({
   }
 }))(TableRow);
 
-// function createData(name, calories, fat, carbs, protein) {
-//   return { name, calories, fat, carbs, protein };
-// }
-
-// const rows = [
-//   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-//   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-//   createData('Eclair', 262, 16.0, 24, 6.0),
-//   createData('Cupcake', 305, 3.7, 67, 4.3),
-//   createData('Gingerbread', 356, 16.0, 49, 3.9)
-// ];
-
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1
@@ -100,7 +88,7 @@ export default function ProductShowList(userMembers) {
   };
 
   const handleSearch = () => {
-    const filterResponse = users.filter(
+    const filterResponse = userMembers.userMembers.filter(
       (element) =>
         element.firstname.toLowerCase() === searchInfo ||
         element.lastname.toLowerCase() === searchInfo ||
@@ -110,6 +98,7 @@ export default function ProductShowList(userMembers) {
           searchInfo
     );
     setUsers(filterResponse);
+    setPage(0);
   };
   const handleOnKeyDown = (event) => {
     if (event.code === 'Enter') {
