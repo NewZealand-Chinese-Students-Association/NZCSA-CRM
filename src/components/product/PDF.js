2a34deb6fff7b9a32d31c63dc5a2e385233bc504
MeloGuan <meloguan123@gmail.com>
2021-08-10 13:56:43 +1200

    updated pdf font size

--- src/components/product/PDF.js
+++ src/components/product/PDF.js
@@ -5,14 +5,20 @@ const PDF = (users) => {
   let y = 10;
   const doc = new jsPDF();
 
+  doc.setFontSize(10);
+
   users.forEach((element) => {
     const name = `${element.firstname} ${element.lastname}`;
     doc.text(name, 10, y);
     doc.text(element.email, 70, y);
     doc.text('Check in: ', 150, y);
     y += 5;
-    doc.text('----'.repeat(50), 0, y);
+    doc.text('----'.repeat(80), 0, y);
     y += 10;
+    if (y > 300) {
+      doc.addPage();
+      y = 10;
+    }
   });
 
   //   doc.text('Hello world!', 10, 10);
