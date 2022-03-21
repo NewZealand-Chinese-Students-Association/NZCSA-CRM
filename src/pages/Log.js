04aaac474fcdaa3a1faf2177622f853349172491
Tony Cui <Tonycui02@gmail.com>
2022-03-21 17:04:48 +1300

    Added code to connect to backend api

--- src/pages/Log.js
+++ src/pages/Log.js
@@ -15,48 +15,20 @@ const Log = () => {
   };
   const [logs, setLogs] = useState([]);
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     setLoading(true);
-  //     axios
-  //       .get(
-  //         'https://nzcsa-backend.herokuapp.com/api/admin/show-log-info',
-  //         config
-  //       )
-  //       .then((res) => {
-  //         setLoading(false);
-  //         setLogs(res.data.reverse());
-  //       });
-  //   };
-  //   fetchData();
-  // }, []);
-
-  // Temporary data before connecting to log api
-  const dummyData = [
-    {
-      _id: {
-        $oid: '614d5ed57a181958fcc658bb'
-      },
-      operator: 'alex',
-      event: 'promoted user',
-      name: 'test',
-      id: '6132f66e0d5c8500047d1202',
-      time: new Date().getTime()
-    },
-    {
-      _id: {
-        $oid: '614d64eae7d7bc528cd25801'
-      },
-      operator: 'alex',
-      event: 'removed user',
-      name: 'test',
-      id: '6132f66e0d5c8500047d1202',
-      time: new Date().getTime()
-    }
-  ];
-
   useEffect(() => {
-    setLogs(dummyData);
+    const fetchData = async () => {
+      setLoading(true);
+      axios
+        .get(
+          'https://nzcsa-backend.herokuapp.com/api/admin/show-log-info',
+          config
+        )
+        .then((res) => {
+          setLoading(false);
+          setLogs(res.data.reverse());
+        });
+    };
+    fetchData();
   }, []);
 
   return (
